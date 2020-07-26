2cb894a450fdf465e32d4bd00c969a90605c3903
TheShubham-K <subham.kumar03@gmail.com>
2020-07-26 09:31:22 +0530

    Updates

---
+++ precache-manifest.4b4e7fd6f9a50d9ad2431549e27da250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dbacd5279cf8db78ab86273031530f",
+    "url": "/Corona_tracker/index.html"
+  },
+  {
+    "revision": "69a971f0639bbede573f",
+    "url": "/Corona_tracker/static/css/main.0753d61c.chunk.css"
+  },
+  {
+    "revision": "795db8cf2b0198851fb9",
+    "url": "/Corona_tracker/static/js/2.e71d28dd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona_tracker/static/js/2.e71d28dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a971f0639bbede573f",
+    "url": "/Corona_tracker/static/js/main.4f5149c0.chunk.js"
+  },
+  {
+    "revision": "81c9f3d9d5c1bb13a945",
+    "url": "/Corona_tracker/static/js/runtime-main.652ee142.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona_tracker/static/media/image.d7265326.png"
+  }
+]);
